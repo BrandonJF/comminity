1e0bd79df1e31eefd0663d6b598e620f016c1552
Brandon John-Freso <brandonjf@gmail.com>
2022-09-21 06:17:50 -0400

    Dockerize and nginx the for deploy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,22 @@
 import * as express from 'express';
+import {createProxyMiddleware} from 'http-proxy-middleware'
 // import { Message } from '@digitalmischief/api-interfaces';
 import { addTodoRoutes } from './app/todos';
 
 const app = express();
+// const apiProxy = createProxyMiddleware({ target: 'http://localhost:' + process.env.APIPORT, changeOrigin: true })
+
 
 // const greeting: Message = { message: 'Welcome to api!' };
 
 // app.get('/api', (req, res) => {
 //   res.send(greeting);
 // });
-
+// app.use('/api', apiProxy)
 addTodoRoutes(app);
 
-const port = process.env.port || 3333;
+const port = process.env.APIPORT //|| 3334;
 const server = app.listen(port, () => {
-  console.log('Listening at http://localhost:' + port + '/api');
+  console.log('API Listening at http://localhost:' + port + '/api');
 });
 server.on('error', console.error);
